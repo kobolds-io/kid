f8ba2f70525d3ccf351a90013f7568b283b47372
Ben Garis-Hodge <bengarishodge@pm.me>
2026-04-18 09:28:33 -0500

    fix benchmarks

--- benchmarks/kid.zig
+++ benchmarks/kid.zig
@@ -6,39 +6,23 @@ const testing = std.testing;
 
 const kid = @import("kid");
 
-pub fn BenchmarkKIDGenerateOne(_: std.mem.Allocator) void {
-    _ = kid.generate();
-}
-
-pub fn BenchmarkKIDGenerate1000(_: std.mem.Allocator) void {
-    for (0..1_000) |_| {
-        _ = kid.generate();
+pub const BenchmarkKIDGenerate = struct {
+    const Self = @This();
+    io: std.Io,
+    iters: usize = 1,
+    pub fn new(io: std.Io, iters: usize) Self {
+        return Self{
+            .io = io,
+            .iters = iters,
+        };
     }
-}
 
-pub fn BenchmarkKIDGenerate8192(_: std.mem.Allocator) void {
-    for (0..8192) |_| {
-        _ = kid.generate();
+    pub fn run(self: *Self, _: std.mem.Allocator) void {
+        for (0..self.iters) |_| {
+            _ = kid.generate(self.io);
+        }
     }
-}
-
-pub fn BenchmarkKIDGenerate10000(_: std.mem.Allocator) void {
-    for (0..10_000) |_| {
-        _ = kid.generate();
-    }
-}
-
-pub fn BenchmarkKIDGenerate100000(_: std.mem.Allocator) void {
-    for (0..100_000) |_| {
-        _ = kid.generate();
-    }
-}
-
-pub fn BenchmarkKIDGenerate1000000(_: std.mem.Allocator) void {
-    for (0..1_000_000) |_| {
-        _ = kid.generate();
-    }
-}
+};
 
 test "KID benchmarks" {
     var bench = zbench.Benchmark.init(std.testing.allocator, .{
@@ -46,21 +30,28 @@ test "KID benchmarks" {
     });
     defer bench.deinit();
 
+    const io = testing.io;
+
     kid.configure(100, .{});
 
-    try bench.add("generate 1 ids", BenchmarkKIDGenerateOne, .{});
-    try bench.add("generate 1000 ids", BenchmarkKIDGenerate1000, .{});
-    try bench.add("generate 8192 ids", BenchmarkKIDGenerate8192, .{});
-    try bench.add("generate 10_000 ids", BenchmarkKIDGenerate10000, .{});
-    try bench.add("generate 100_000 ids", BenchmarkKIDGenerate100000, .{});
-    try bench.add("generate 1_000_000 ids", BenchmarkKIDGenerate1000000, .{ .iterations = 5 });
+    try bench.addParam("generate 1 ids", &BenchmarkKIDGenerate.new(io, 1), .{});
+    try bench.addParam("generate 1000 ids", &BenchmarkKIDGenerate.new(io, 1_000), .{});
+    try bench.addParam("generate 8192 ids", &BenchmarkKIDGenerate.new(io, 8192), .{});
+    try bench.addParam("generate 10_000 ids", &BenchmarkKIDGenerate.new(io, 10_000), .{});
+    try bench.addParam("generate 100_000 ids", &BenchmarkKIDGenerate.new(io, 100_000), .{});
+    try bench.addParam(
+        "generate 1_000_000 ids",
+        &BenchmarkKIDGenerate.new(io, 1_000_000),
+        .{ .iterations = 5 },
+    );
 
-    var stderr = std.fs.File.stderr().writerStreaming(&.{});
-    const writer = &stderr.interface;
+    const stderr = std.Io.File.stderr();
+    var stderr_writer = stderr.writerStreaming(io, &.{});
+    const writer = &stderr_writer.interface;
 
     try writer.writeAll("\n");
     try writer.writeAll("|----------------|\n");
     try writer.writeAll("| KID Benchmarks |\n");
     try writer.writeAll("|----------------|\n");
-    try bench.run(writer);
+    try bench.run(io, stderr);
 }
--- benchmarks/other.zig
+++ benchmarks/other.zig
@@ -7,73 +7,41 @@ const testing = std.testing;
 
 const kid = @import("kid");
 
-pub fn BenchmarkUUIDV7GenerateOne(_: std.mem.Allocator) void {
-    _ = uuid.v7.new();
-}
-
-pub fn BenchmarkUUIDV7Generate1000(_: std.mem.Allocator) void {
-    for (0..1_000) |_| {
-        _ = uuid.v7.new();
+pub const BenchmarkUUIDv7Generate = struct {
+    const Self = @This();
+    io: std.Io,
+    iters: usize = 1,
+    pub fn new(io: std.Io, iters: usize) Self {
+        return Self{
+            .io = io,
+            .iters = iters,
+        };
     }
-}
 
-pub fn BenchmarkUUIDV7Generate8192(_: std.mem.Allocator) void {
-    for (0..8192) |_| {
-        _ = uuid.v7.new();
+    pub fn run(self: *Self, _: std.mem.Allocator) void {
+        for (0..self.iters) |_| {
+            _ = uuid.v7.new(self.io);
+        }
     }
-}
+};
 
-pub fn BenchmarkUUIDV7Generate10000(_: std.mem.Allocator) void {
-    for (0..10_000) |_| {
-        _ = uuid.v7.new();
+pub const BenchmarkUUIDv4Generate = struct {
+    const Self = @This();
+    io: std.Io,
+    iters: usize = 1,
+    pub fn new(io: std.Io, iters: usize) Self {
+        return Self{
+            .io = io,
+            .iters = iters,
+        };
     }
-}
 
-pub fn BenchmarkUUIDV7Generate100000(_: std.mem.Allocator) void {
-    for (0..100_000) |_| {
-        _ = uuid.v7.new();
+    pub fn run(self: *Self, _: std.mem.Allocator) void {
+        for (0..self.iters) |_| {
+            _ = uuid.v4.new(self.io);
+        }
     }
-}
-
-pub fn BenchmarkUUIDV7Generate1000000(_: std.mem.Allocator) void {
-    for (0..1_000_000) |_| {
-        _ = uuid.v7.new();
-    }
-}
-
-pub fn BenchmarkUUIDV4GenerateOne(_: std.mem.Allocator) void {
-    _ = uuid.v4.new();
-}
-
-pub fn BenchmarkUUIDV4Generate1000(_: std.mem.Allocator) void {
-    for (0..1_000) |_| {
-        _ = uuid.v4.new();
-    }
-}
-
-pub fn BenchmarkUUIDV4Generate8192(_: std.mem.Allocator) void {
-    for (0..8192) |_| {
-        _ = uuid.v4.new();
-    }
-}
-
-pub fn BenchmarkUUIDV4Generate10000(_: std.mem.Allocator) void {
-    for (0..10_000) |_| {
-        _ = uuid.v4.new();
-    }
-}
-
-pub fn BenchmarkUUIDV4Generate100000(_: std.mem.Allocator) void {
-    for (0..100_000) |_| {
-        _ = uuid.v4.new();
-    }
-}
-
-pub fn BenchmarkUUIDV4Generate1000000(_: std.mem.Allocator) void {
-    for (0..1_000_000) |_| {
-        _ = uuid.v4.new();
-    }
-}
+};
 
 test "uuid.v7 benchmarks" {
     var bench = zbench.Benchmark.init(std.testing.allocator, .{
@@ -81,21 +49,28 @@ test "uuid.v7 benchmarks" {
     });
     defer bench.deinit();
 
-    try bench.add("uuid.v7 1 ids", BenchmarkUUIDV7GenerateOne, .{});
-    try bench.add("uuid.v7 1000 ids", BenchmarkUUIDV7Generate1000, .{});
-    try bench.add("uuid.v7 8192 ids", BenchmarkUUIDV7Generate8192, .{});
-    try bench.add("uuid.v7 10_000 ids", BenchmarkUUIDV7Generate10000, .{});
-    try bench.add("uuid.v7 100_000 ids", BenchmarkUUIDV7Generate100000, .{});
-    try bench.add("uuid.v7 1_000_000 ids", BenchmarkUUIDV7Generate1000000, .{ .iterations = 5 });
+    const io = testing.io;
 
-    var stderr = std.fs.File.stderr().writerStreaming(&.{});
-    const writer = &stderr.interface;
+    try bench.addParam("uuid.v7 1 ids", &BenchmarkUUIDv7Generate.new(io, 1), .{});
+    try bench.addParam("uuid.v7 1000 ids", &BenchmarkUUIDv7Generate.new(io, 1_000), .{});
+    try bench.addParam("uuid.v7 8192 ids", &BenchmarkUUIDv7Generate.new(io, 8192), .{});
+    try bench.addParam("uuid.v7 10_000 ids", &BenchmarkUUIDv7Generate.new(io, 10_000), .{});
+    try bench.addParam("uuid.v7 100_000 ids", &BenchmarkUUIDv7Generate.new(io, 100_000), .{});
+    try bench.addParam(
+        "uuid.v7 1_000_000 ids",
+        &BenchmarkUUIDv7Generate.new(io, 1_000_000),
+        .{ .iterations = 5 },
+    );
+
+    const stderr = std.Io.File.stderr();
+    var stderr_writer = stderr.writerStreaming(io, &.{});
+    const writer = &stderr_writer.interface;
 
     try writer.writeAll("\n");
     try writer.writeAll("|--------------------|\n");
     try writer.writeAll("| uuid.v7 Benchmarks |\n");
     try writer.writeAll("|--------------------|\n");
-    try bench.run(writer);
+    try bench.run(io, stderr);
 }
 
 test "uuid.v4 benchmarks" {
@@ -104,19 +79,26 @@ test "uuid.v4 benchmarks" {
     });
     defer bench.deinit();
 
-    try bench.add("uuid.v4 1 ids", BenchmarkUUIDV4GenerateOne, .{});
-    try bench.add("uuid.v4 1000 ids", BenchmarkUUIDV4Generate1000, .{});
-    try bench.add("uuid.v4 8192 ids", BenchmarkUUIDV4Generate8192, .{});
-    try bench.add("uuid.v4 10_000 ids", BenchmarkUUIDV4Generate10000, .{});
-    try bench.add("uuid.v4 100_000 ids", BenchmarkUUIDV4Generate100000, .{});
-    try bench.add("uuid.v4 1_000_000 ids", BenchmarkUUIDV4Generate1000000, .{ .iterations = 5 });
+    const io = testing.io;
 
-    var stderr = std.fs.File.stderr().writerStreaming(&.{});
-    const writer = &stderr.interface;
+    try bench.addParam("uuid.v4 1 ids", &BenchmarkUUIDv4Generate.new(io, 1), .{});
+    try bench.addParam("uuid.v4 1000 ids", &BenchmarkUUIDv4Generate.new(io, 1_000), .{});
+    try bench.addParam("uuid.v4 8192 ids", &BenchmarkUUIDv4Generate.new(io, 8192), .{});
+    try bench.addParam("uuid.v4 10_000 ids", &BenchmarkUUIDv4Generate.new(io, 10_000), .{});
+    try bench.addParam("uuid.v4 100_000 ids", &BenchmarkUUIDv4Generate.new(io, 100_000), .{});
+    try bench.addParam(
+        "uuid.v4 1_000_000 ids",
+        &BenchmarkUUIDv4Generate.new(io, 1_000_000),
+        .{ .iterations = 5 },
+    );
+
+    const stderr = std.Io.File.stderr();
+    var stderr_writer = stderr.writerStreaming(io, &.{});
+    const writer = &stderr_writer.interface;
 
     try writer.writeAll("\n");
     try writer.writeAll("|--------------------|\n");
     try writer.writeAll("| uuid.v4 Benchmarks |\n");
     try writer.writeAll("|--------------------|\n");
-    try bench.run(writer);
+    try bench.run(io, stderr);
 }
